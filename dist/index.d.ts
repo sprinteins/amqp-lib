e8e8a21a6af3a454ad114ef11b16c66fa60b4a29
Ali <me0019@gmail.com>
2020-02-10 12:24:45 +0100

    Remove dist folder from repo

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Connection } from "./connection";
-export { Exchange } from "./exchange";
-export { Message, MessageProperties, MessageType } from "./message";
-export { Queue } from "./queue";
